11a30bef5ff643d99e2d57749bde61cea3cd8385
Ryna <pinktulips@Hotmail.com>
2021-07-28 11:28:05 -0700

    save

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,24 @@
 class Song < ActiveRecord::Base
-  # add associations here
+  has_many :notes
+  belongs_to :genre
+  belongs_to :artist
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
+
+  def create
+    Song.create(artist_name_params)
+  end
+
+  private
+
+  def artist_name_params
+    params.require(:song).permit(:artist_name, :artist_name)
+  end  
+
 end
